a16ec95b2850b2aa8c0bd4fe82352ad36f4cbae6
dzming-git <dzm_work@163.com>
2023-03-11 00:43:37 +0800

    兼容DBot_SDK0.2版本的修改

--- example/app/services/service.py
+++ example/app/services/service.py
@@ -7,37 +7,14 @@ def countdown(gid=None, qid=None, msg_list=[]):
         send_message(f'倒计时 {time_countdown}', gid, qid)
         time_countdown -= 1
         time.sleep(1)
-    message_parts = []
-    message_parts.append('倒计时 结束')
-    message_send = '\n'.join(message_parts).rstrip('\n')
-    return message_send
+    send_message('倒计时 结束', gid, qid)
+
 
 def func2(gid=None, qid=None, msg_list=[]):
-    message_parts = []
-    message_parts.append('This is func2')
-    message_send = '\n'.join(message_parts).rstrip('\n')
-    return message_send
+    send_message('This is func2', gid, qid)
 
 def func3(gid=None, qid=None, msg_list=[]):
-    message_parts = []
-    message_parts.append('This is func3')
-    message_send = '\n'.join(message_parts).rstrip('\n')
-    return message_send
-
-def add(gid=None, qid=None, msg_list=[]):
-    message_parts = []
-    try:
-        int_list = list(map(int, msg_list))
-        sum_of_ints = sum(int_list)
-        message_str = ''
-        for num in int_list:
-            message_str += f'{num} + '
-        message_str = message_str[0:-2] + f'= {sum_of_ints}'
-        message_parts.append(message_str)
-    except:
-        message_parts.append('input error')
-    message_send = '\n'.join(message_parts).rstrip('\n')
-    return message_send
+    send_message('This is func3', gid, qid)
 
 func_dict = {
     '#倒计时': {
