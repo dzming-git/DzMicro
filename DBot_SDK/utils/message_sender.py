a75c310911a09a599c23ef9392db2df06361c029
dzming-git <dzm_work@163.com>
2023-03-10 20:01:22 +0800

    向平台程序传送消息的endpoint名字修改为service_message

--- DBot_SDK/utils/message_sender.py
+++ DBot_SDK/utils/message_sender.py
@@ -5,7 +5,7 @@ def send_message_to_message_broker(message, gid=None, qid=None):
     from DBot_SDK.conf.route_info import RouteInfo
     ip = RouteInfo.get_message_broker_ip()
     port = RouteInfo.get_message_broker_port()
-    endport = RouteInfo.get_message_broker_endpoint('service_results')
+    endport = RouteInfo.get_message_broker_endpoint('service_message')
     url = f'http://{ip}:{port}/{endport}'
     response = requests.post(url, json={'message': message, 'gid': gid, 'qid': qid})
     return response.json()
