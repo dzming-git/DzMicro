6a30396964232bba5cc88d5a4750f0fbe5ddfe4b
dzming-git <dzm_work@163.com>
2023-03-14 20:55:01 +0800

    完善了例程中的一些服务响应函数

--- example/app/services/service.py
+++ example/app/services/service.py
@@ -1,16 +1,22 @@
 import time
 from DBot_SDK import send_message
 
-def default_func(gid=None, qid=None, msg_list=[]):
-    send_message('默认回复，待开发', gid, qid)
+def help(gid=None, qid=None, msg_list=[]):
+    message = '该关键词支持指令有：\n'
+    for command in list(func_dict.keys()):
+        message += f'{command}\n'
+    send_message(message.strip('\n'), gid, qid)
 
 def countdown(gid=None, qid=None, msg_list=[]):
-    time_countdown = int(msg_list[0])
-    while time_countdown > 0:
-        send_message(f'倒计时 {time_countdown}', gid, qid)
-        time_countdown -= 1
-        time.sleep(1)
-    send_message('倒计时 结束', gid, qid)
+    if not msg_list:
+        send_message('缺少参数', gid, qid)
+    else:
+        time_countdown = int(msg_list[0])
+        while time_countdown > 0:
+            send_message(f'倒计时 {time_countdown}', gid, qid)
+            time_countdown -= 1
+            time.sleep(1)
+        send_message('倒计时 结束', gid, qid)
 
 
 def func2(gid=None, qid=None, msg_list=[]):
@@ -20,8 +26,8 @@ def func3(gid=None, qid=None, msg_list=[]):
     send_message('This is func3', gid, qid)
 
 func_dict = {
-    'default':{
-        'func': default_func,
+    '帮助':{
+        'func': help,
         'permission': 'USER'
         },
     '倒计时': {
