871c7a7f0715993c940bc52eeffec414287784dc
dzming-git <dzm_work@163.com>
2023-03-16 01:34:05 +0800

    修复了连接错误时，无法获取gid qid的问题

--- DBot_SDK/app/message_handler/message_handler.py
+++ DBot_SDK/app/message_handler/message_handler.py
@@ -20,19 +20,33 @@ class MessageHandlerThread(threading.Thread):
             message = self.message_queue.get(block=True)
             url = message['url']
             json = message['json']
+            gid = json['gid']
+            qid = json['qid']
             try:
                 response = requests.post(url, json=json)
                 result_dict = response.json()
                 permission = result_dict['permission']
-                gid = json['gid']
-                qid = json['qid']
-                if not permission:
+                # None不处理，False告知权限不足
+                if permission is False:
                     permission_denied(gid=gid, qid=qid)
                 print(f"Message forwarded to {url}")
                 time.sleep(0.1)
             except:
-                send_message_to_cqhttp('连接错误', gid, qid)    
+                send_message_to_cqhttp('连接错误', gid, qid)
     
+    def add_message_queue(self, service_name, command, args, gid, qid, is_user_call):
+        service_info = ServiceRegistry.get_service(service_name)
+        if service_info is not None:
+            service_ip = service_info['ip']
+            service_port = service_info['port']
+            endpoint = service_info['endpoints']['receive_command']
+            url = f"http://{service_ip}:{service_port}/{endpoint}"
+            message_json = {
+                'url':url,
+                'json': {'command': command, 'args': args, 'gid': gid, 'qid': qid, 'is_user_call': is_user_call}
+            }
+            self.message_queue.put(message_json)
+
     def message_handler(self, message: str, gid=None, qid=None):
         sends = []
         def message_split(message):
@@ -51,6 +65,7 @@ class MessageHandlerThread(threading.Thread):
                 return None, None, None
         keywords = list(BotCommands.get_keywords())
         keyword, command, args = message_split(message)
+        #TODO 含有关键词的不按照监听的方式转发，未来可能有监听指令的功能，待完善
         if keyword:
             if keyword not in keywords:
                 keyword_error_handler(gid, qid)
@@ -59,34 +74,16 @@ class MessageHandlerThread(threading.Thread):
                 if command not in commands:
                     command_error_handler(gid, qid)
                 service_name = BotCommands.get_service_name(keyword)
-                sends.append((service_name, command, args))
+                self.add_message_queue(service_name, command, args, gid, qid, True)
+                return
         # 监听消息转发
-        #TODO 正常的指令是否需要处理？
         listens = ServiceRegistry.get_listens()
         for listen in listens:
             service_name = listen.get('service_name')
             command = listen.get('command')
             listen_gid = listen.get('gid')
             listen_qid = listen.get('qid')
-            #TODO 可能存在问题，感觉for循环应该就只有一个或0个，实现的不优雅
             if gid == listen_gid:
-                if sends:
-                    service_name_in_sends, command_in_sends, args_in_sends = sends[0]
-                    if service_name != service_name_in_sends or command != command_in_sends:
-                        sends.append((service_name, command, [message]))
-                else:
-                    sends.append((service_name, command, [message]))
-        for service_name, command, args in sends:
-            service_info = ServiceRegistry.get_service(service_name)
-            if service_info is not None:
-                service_ip = service_info['ip']
-                service_port = service_info['port']
-                endpoint = service_info['endpoints']['receive_command']
-                url = f"http://{service_ip}:{service_port}/{endpoint}"
-                message_json = {
-                    'url':url,
-                    'json': {'command': command, 'args': args, 'gid': gid, 'qid': qid}
-                }
-                self.message_queue.put(message_json)
+                self.add_message_queue(service_name, command, [message], gid, qid, False)
 
 message_handler_thread = MessageHandlerThread()
