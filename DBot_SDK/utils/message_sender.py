ddf0073951f5631bc365f24f755334b8bc4c5dcb
dzming-git <dzm_work@163.com>
2023-03-16 11:32:44 +0800

    修复了发送消息中包含特殊字符时，无法正常发送的问题

--- DBot_SDK/utils/message_sender.py
+++ DBot_SDK/utils/message_sender.py
@@ -1,5 +1,6 @@
 # message_sender.py
 import requests
+import urllib
 
 def send_message_to_message_broker(message, gid=None, qid=None):
     from DBot_SDK.conf.route_info import RouteInfo
@@ -11,8 +12,9 @@ def send_message_to_message_broker(message, gid=None, qid=None):
     return response.json()
 
 # message broker专用
-def send_message_to_cqhttp(message, gid=None, qid=None):
+def send_message_to_cqhttp(message: str, gid=None, qid=None):
+    urlencoded_message = urllib.parse.quote(message)
     if gid is None:
-        requests.get(f'http://127.0.0.1:5700/send_private_msg?user_id={qid}&message={message}')
+        requests.get(f'http://127.0.0.1:5700/send_private_msg?user_id={qid}&message={urlencoded_message}')
     else:   
-        requests.get(f'http://127.0.0.1:5700/send_group_msg?group_id={gid}&message={message}')
+        requests.get(f'http://127.0.0.1:5700/send_group_msg?group_id={gid}&message={urlencoded_message}')
