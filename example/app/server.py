b59d3c95e0dfa1a8a623b2a7410ede58683de1e8
dzming-git <dzm_work@163.com>
2023-03-18 16:41:24 +0800

    例程中优化了SDK接口的使用位置

--- example/app/server.py
+++ example/app/server.py
@@ -1,17 +1,17 @@
 # service.py
 import time
-from DBot_SDK import ConfigFromUser, server_thread
 from app import func_dict, KEYWORD
 
-def load_conf():
-    ConfigFromUser.Authority_load_config('conf/authority/authority.yaml')
-    ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
-    ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
+from DBot_SDK import ConfigFromUser
+ConfigFromUser.Authority_load_config('conf/authority/authority.yaml')
+ConfigFromUser.RouteInfo_load_config('conf/route_info/route_info.yaml')
+ConfigFromUser.ConsulInfo_load_config('conf/consul_info/consul_info.yaml')
+ConfigFromUser.set_keyword(KEYWORD)
+ConfigFromUser.set_func_dict(func_dict)
+
+from DBot_SDK import server_thread
 
 if __name__ == '__main__': 
-    load_conf()
-    ConfigFromUser.set_keyword(KEYWORD)
-    ConfigFromUser.set_func_dict(func_dict)
     server_thread.set_safe_start(True)
     if server_thread.start():
         while True:
