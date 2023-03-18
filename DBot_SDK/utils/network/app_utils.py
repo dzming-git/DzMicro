1527bc882b2dd7278ce74a17bd3d0d6239995f8c
dzming-git <dzm_work@163.com>
2023-03-18 16:15:02 +0800

    去除不必要的代码；延迟导入防止循环导入

--- DBot_SDK/utils/network/app_utils.py
+++ DBot_SDK/utils/network/app_utils.py
@@ -1,13 +1,10 @@
 import requests
 import json
-from DBot_SDK.utils import consul_client
 
 def upload_service_commands():
     # 注册支持的指令到消息代理程序
     from DBot_SDK.conf import RouteInfo
     from DBot_SDK.app import FuncDict
-    message_broker_ip = RouteInfo.get_message_broker_ip()
-    message_broker_port = RouteInfo.get_message_broker_port()
     service_name = RouteInfo.get_service_name()
     keyword = FuncDict.get_keyword()
     commands = FuncDict.get_commands()
@@ -16,6 +13,7 @@ def upload_service_commands():
         'keyword': keyword,
         'commands': commands
     })
+    from DBot_SDK.utils import consul_client
     consul_client.update_key_value({k: v})
 
 def publish_task(ip, port, json):
