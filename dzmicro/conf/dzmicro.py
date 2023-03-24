81facc570c836a034cb5ffb4c66ab84b6f8dcd2b
dzming-git <dzm_work@163.com>
2023-03-24 10:37:15 +0800

    添加用户接口函数的输入参数与返回值的类型限制

--- dzmicro/conf/dzmicro.py
+++ dzmicro/conf/dzmicro.py
@@ -1,4 +1,4 @@
-from typing import List, Callable
+from typing import List, Callable, Dict
 
 class DzMicro:
     def __init__(self) -> None:
@@ -6,7 +6,7 @@ class DzMicro:
         self._server_thread = server_thread
         self._is_platform = False
 
-    def is_platform(self, confirm=None):
+    def is_platform(self, confirm: bool = None) -> bool:
         '''
         查询/确认是否是平台程序程序
         confirm参数不输入时为查询
@@ -18,34 +18,30 @@ class DzMicro:
         else:
             return self._is_platform
     
-    def set_Authority_config(self, config_path):
+    def set_Authority_config(self, config_path: str) -> None:
         from dzmicro.conf import Authority
         Authority.load_config(config_path)
     
-    def set_RouteInfo_config(self, config_path):
+    def set_RouteInfo_config(self, config_path: str) -> None:
         from dzmicro.conf import RouteInfo
         RouteInfo.load_config(config_path)
     
-    def set_ConsulInfo_config(self, config_path):
+    def set_ConsulInfo_config(self, config_path: str) -> None:
         from dzmicro.conf import ConsulInfo
         ConsulInfo.load_config(config_path)
     
-    def set_func_dict(self, func_dict):
+    def set_func_dict(self, func_dict: Dict[str, Dict[str, any]]) -> None:
         from dzmicro.app import FuncDict
         FuncDict.set_func_dict(func_dict)
     
-    def set_keyword(self, keyword):
+    def set_keyword(self, keyword: str) -> None:
         from dzmicro.app import FuncDict
         FuncDict.set_keyword(keyword)
     
-    def set_send_message_to_source(self, func: Callable[[str, List, bool], None]):
+    def set_send_message_to_source(self, func: Callable[[str, List, bool], None]) -> None:
         from dzmicro.utils.network import MessageSender
         MessageSender.set_send_message_to_source(func)
 
     def start_server(self, safe_start: bool = True) -> bool:
         self._server_thread.set_safe_start(safe_start)
         return self._server_thread.start(self._is_platform)
-    
-    def handle(self, message, source_id):
-        from dzmicro.app.message_handler.message_handler import message_handler_thread
-        message_handler_thread.message_handler(message, source_id)
